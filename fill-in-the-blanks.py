1ea0c00765cdc3d206a1d4aa14840843d7f37b32
a255128 <33543211+a255128@users.noreply.github.com>
2017-12-04 15:42:01 +0800

    1.3
    按照udacity的建議做好了修正,結果遇到謎之問題

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -1,63 +1,76 @@
 #Global Variables:
-easy_quiz='''Most people think that ___1___ invented the first light bulb.They’re wrong.
-In fact, he was spectacularly ___2___ to the game.
-In 1878, when the 36-year-old inventor decided to ___3___ on building a light bulb,
-23 others had already invented early ___4___ called arc lamps.'''
+easy_quiz='''Most people think that Edison invented the first light bulb.They are wrong.
+In fact, he was spectacularly late to the game.
+In 1878, when the 36-year-old inventor decided to focus on building a light bulb,
+23 others had already invented early versions called arc lamps.'''
 
 
-medium_quiz='''Perhaps the most ___1___ current success formula is the 10,000-hour rule
-by Malcolm Gladwell. The idea is that you need 10,000 hours of ___2___
+medium_quiz='''Perhaps the most popular current success formula is the 10,000-hour rule
+by Malcolm Gladwell. The idea is that you need 10,000 hours of deliberate
 practice to become a world-class performer in any field.
-Research now tells us, that this formula is woefully ___3___ to explain success,
-especially in the professional ___4___. '''
+Research now tells us, that this formula is woefully inadequate to explain success,
+especially in the professional realm. '''
 
-hard_quiz='''What Edison and others teach us is that we should ___1___ the number of experiments,
+hard_quiz='''What Edison and others teach us is that we should maximize the number of experiments,
 not hours. Instead of the 10,000-hour rule, we need what I call the 10,000-experiment rule.
-Throughout history, the ___2___ method has arguably produced more human progress than
-any other ___3___. At the heart of the scientific method is experimentation:
-develop a ___4___, perform a test to prove the ___4___ right or wrong, analyze the results,
-and create a new ___4___ based on what you learned.'''
+Throughout history, the scientific method has arguably produced more human progress than
+any other philosophy. At the heart of the scientific method is experimentation:
+develop a hypothesis , perform a test to prove the hypothesis right or wrong, analyze the results,
+and create a new hypothesis based on what you learned.'''
 
 #the full article can find in https://medium.com/the-mission/forget-about-the-10-000-hour-rule-7b7a39343523
 
-lunatic_quiz='''Death is nothing at all. 
-It does not count. 
-I have only ___1___ away into the next room. 
-Nothing has happened. 
+lunatic_quiz='''Death is nothing at all.
+It does not count.
+I have only slipped away into the next room.
+Nothing has happened.
 
-Everything ___2___ exactly as it was. 
-I am I, and you are you, 
-and the old life that we lived so fondly together is untouched, unchanged. 
-Whatever we were to each other, that we are still. 
+Everything remains exactly as it was.
+I am I, and you are you,
+and the old life that we lived so fondly together is untouched, unchanged.
+Whatever we were to each other, that we are still.
 
-Call me by the old familiar name. 
-Speak of me in the easy way which you always used. 
-Put no difference into your tone. 
-Wear no forced air of ___3___ or sorrow.
+Call me by the old familiar name.
+Speak of me in the easy way which you always used.
+Put no difference into your tone.
+Wear no forced air of solemnity or sorrow.
 
 Laugh as we always laughed at the little jokes that we enjoyed together.
-Play, smile, think of me, pray for me. 
-Let my name be ever the ___4___ word that it always was. 
-Let it be spoken without an effort, without the ghost of a shadow upon it. '''
-
+Play, smile, think of me, pray for me.
+Let my name be ever the household word that it always was.
+Let it be spoken without an effort, without the ghost of a shadow upon it.'''
 
 '''Source: https://www.familyfriendpoems.com/poem/death-is-nothing-at-all-by-henry-scott-holland'''
 
 paragraphs = [easy_quiz, medium_quiz, hard_quiz, lunatic_quiz]
 
 answer_of_quizs=[["Edison","late","focus","versions"], #easy_answers
-["popular","deliberate","inadequate","realm"], #medium_answers
+["popular","deliberate","inadequate","professional"], #medium_answers
 ["maximize","scientific","philosophy","hypothesis"], #hard_answers
-["slipped","remains","solemnity","household"]] #luntic_answer
+["slipped","remains","solemnity","household"] #luntic_answer
 
 #Futctions:
 
 def welcome(player_name):
-    #Show player's name.
+        '''
+        Inputs:
+                The name of player.
+        Behavior:
+                Show player's name.
+        Outputs:
+                Introduction to game welcome.
+        '''
     print "Welcome," + player_name +".This is a fill in blanks quiz. Have fun!"
 
 def select_a_difficulty_level():
-    #Player can choose a level they want to play.
+    '''
+    Inputs:
+            Current level.
+    Behavior:
+            Player can choose a level they want to play.
+    Outputs:
+            The specific paragraph and answers associated with that level.
+    '''
     level = raw_input("Enter your difficulty level(easy / medium / hard):")
     if level == "easy":
         print "You are in easy game"
@@ -72,65 +85,81 @@ def select_a_difficulty_level():
         print "You have found the sercet game!"
         return 3
 
-def process_paragraph(para, level, blank):
-    paragraph = para
-     #replace all the answer_of_quizs from current blank to the last one.
-    if blank < len(answer_of_quizs[level]):
+def process_paragraph(level, blank):
+    '''
+    Inputs:
+            Current level and given blank.
+    Behavior:
+            Replace the words in answer_of_quizs for the level from given blank.
+    Outputs:
+            Paragraph show the blank.
+    '''
+    paragraph = paragraphs[level]
+        #replace all answer_of_quizs from current blank to the last one.
+    while blank < len(answer_of_quizs[level]):
         word_to_replace = answer_of_quizs[level][blank]
         list_of_words = paragraph.split(" ")
-        
         index = 0
-       
+         #Replace the word_to_replace with ___number___
         while index < len(list_of_words):
-            temp_blank = "___" + str(blank + 1) + "___"
-            
-            if list_of_words[index] == temp_blank:
-                list_of_words[index] = word_to_replace
+            if list_of_words[index] == word_to_replace:
+                list_of_words[index] = "___" + str(blank + 1) + "___"
             index += 1
 
-        # blank += 1
-        paragraph = " ".join(list_of_words)  
-        return paragraph
+        blank += 1
+        paragraph = " ".join(list_of_words)
 
-    else:
-        return paragraph
+    return paragraph
 
 # play the game for the given level
 def play_game(level):
-    
-    blank = 0
-    temp_pragraph = paragraphs[level]
-    
-    # while not all answer_of_quizs are filled, keep guessing until the correct word is provided.
+    '''
+    Inputs:
+            The current level.
+    Behavior:
+            Collect user's answer and judge it is right or wrong.
+    Outputs:
+            The correct answers replaced blanks in paragraph.
+    '''
+    blank = 0 # Given blank
+
     while blank < len(answer_of_quizs[level]):
-        
-        print temp_pragraph
+        print process_paragraph(level, blank)
+
         question = "\n What should go in blank " + str(blank + 1) + "?"
         answer = raw_input(question).lower()
 
         correct_answer = answer_of_quizs[level][blank].lower()
-
         if correct_answer == answer:
-            temp_pragraph = process_paragraph(temp_pragraph, level, blank)
             print "Correct!"
             blank += 1
+            #The correct answers replaced blanks in paragraph.
         else:
             print "Try again!"
 
     print paragraphs[level]
     print "Well played!"
 
-def play_game():
-    #Execute the main program.
+def main():
+    '''
+    Inputs:
+            Click Run Module in Python.
+    Behavior:
+            Execute the main program.
+    Outputs:
+            Whole program.
+    '''
     player_name = raw_input("What is your name? ")
     welcome(player_name)
 
     level = select_a_difficulty_level()
+    max_level=4
+    able_to_increase_difficult_level=3
 
-    while level < 4:
+    while level < max_level:
         play_game(level)
 
-        if level < 3:
+        if level < able_to_increase_difficult_level:
             proceed = raw_input("Too easy? Try harder quiz! (YES/NO)").upper()
             if proceed == "YES":
                 level += 1
@@ -139,6 +168,6 @@ def play_game():
         else:
             break
 
-    print player_name + ", Thanks for playing! "
+    print player_name + ",Thanks for playing! "
 
-    play_game()
+main()
